616f0d1a48bb1134279be8a99425c7ff8d6f3b7f
markakash <akashniki@gmail.com>
2024-01-21 06:25:07 +0530

    munch: Use 8Gb dalvik heap size config
     - Helps fix java.lang.OutOfMemoryError: Failed to allocate a 24 byte allocation with 2582544 free bytes and 2522KB until OOM, target footprint 268435456, growth limit 268435456; giving up on allocation because <1% of heap free after GC
    
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, vendor/xiaomi/miuicamera/miuicamera.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
