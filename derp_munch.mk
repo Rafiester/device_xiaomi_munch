463dd8d6979383d6d246e19fd867c1fc699e729b
Rafiester <rafi.haqqi@gmail.com>
2024-02-10 02:29:02 +0700

    munch: Welcome DerpFest!
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- derp_munch.mk
+++ derp_munch.mk
@@ -11,11 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common DerpFest configurations
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Pixel Stuff
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_munch
+PRODUCT_NAME := derp_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
